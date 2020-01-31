4ec279ed9bd35c3157a4edf6321d3e8186166495
Ferdz <ferdz.steenkamp@gmail.com>
2020-01-31 08:07:09 +0200

    initial commit

---
+++ example/set_value_example.dart
@@ -0,0 +1,6 @@
+import 'package:set_value/set_value.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/set_value.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library set_value;
+
+export 'src/set_value_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
