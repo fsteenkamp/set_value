25221ad79d640ffed65b3bea540087dc8cb5ecb9
Ferdz <ferdz.steenkamp@gmail.com>
2020-01-31 21:21:54 +0200

    added tests

--- test/set_value_test.dart
+++ test/set_value_test.dart
@@ -2,15 +2,87 @@ import 'package:set_value/set_value.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {
-    Awesome awesome;
+  group('Test group', () {
 
-    setUp(() {
-      awesome = Awesome();
+    test('Test set function', () {
+      var setValue = SetValue();
+      var testMap = <String, dynamic>{
+        'Key 1': 'Val 1',
+        'Key 2': 'Val 2',
+        'Key 3': {
+          'Key 3.1': 'Val 3'
+        }
+      };
+
+      testMap = setValue.set(testMap, ['Key 4','Key 4.1','Key 4.1.1'], 'Value 4');
+      testMap = setValue.set(testMap, ['Key 5'], [
+        1,
+        2
+      ]);
+
+      var assertTest = <String, dynamic>{
+        'Key 1': 'Val 1',
+        'Key 2': 'Val 2',
+        'Key 3': {
+          'Key 3.1': 'Val 3'
+        },
+        'Key 4': {
+          'Key 4.1': {
+            'Key 4.1.1': 'Value 4',
+          }
+        },
+        'Key 5': [
+          1, 2
+        ]
+      };
+
+      expect(testMap, equals(assertTest));
     });
 
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
+    test('Test unset function', () {
+      var setValue = SetValue();
+      var testMap = <String, dynamic>{
+        'Key 1': 'Val 1',
+        'Key 2': 'Val 2',
+        'Key 3': {
+          'Key 3.1': 'Val 3'
+        },
+        'Key 5': {
+          'Key 5.1': {
+            'Key 5.1.2': [
+              1,
+              2
+            ]
+          },
+          'Key 5.2': {
+            'Key 5.2.1': 'Val 5.2.1',
+            'Key 5.2.2': 'Val 5.2.2'
+          }
+        }
+      };
+
+      testMap = setValue.unset(testMap, ['Key 5','Key 5.2','Key 5.2.2']);
+      testMap = setValue.unset(testMap, ['Key 1']);
+
+      var assertTest = <String, dynamic>{
+        'Key 2': 'Val 2',
+        'Key 3': {
+          'Key 3.1': 'Val 3'
+        },
+        'Key 5': {
+          'Key 5.1': {
+            'Key 5.1.2': [
+              1,
+              2
+            ]
+          },
+          'Key 5.2': {
+            'Key 5.2.1': 'Val 5.2.1',
+          }
+        }
+      };
+
+      expect(testMap, equals(assertTest));
     });
   });
 }
