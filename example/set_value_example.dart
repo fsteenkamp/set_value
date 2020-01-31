87d023b3d143aeca61f756c838ae79a93ffa7dcf
Ferdz <ferdz.steenkamp@gmail.com>
2020-01-31 19:40:42 +0200

    everything seems to be working

--- example/set_value_example.dart
+++ example/set_value_example.dart
@@ -1,6 +1,50 @@
 import 'package:set_value/set_value.dart';
+import 'dart:convert';
 
 void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  var mock = <String, dynamic>{
+    'Key 1': 'Val 1',
+    'Key 2': 'Val 2',
+    'Key 3': {
+      'Key 3.1': 'Val 3'
+    }
+  };
+
+  var setValue = SetValue();
+
+  /*
+  * Below we set three different keys on the map,
+  * two of which are nested.
+  * */
+
+  mock = setValue.set(mock, ['Key 4'], 'Val 4');
+  mock = setValue.set(mock, ['Key 5','Key 5.1'], 'Val 5.1');
+  mock = setValue.set(mock, ['Key 5','Key 5.2'], 'Val 5.2');
+
+  /*
+  * Below we remove Key 2 and it's value from the map
+  * */
+
+  mock = setValue.unset(mock, ['Key 2']);
+
+
+  /*
+  * Here we expect an output of:
+  * {
+  *   'Key 1': 'Val 1',
+  *   'Key 3': {
+  *     'Key 3.1': 'Val 3'
+  *   }
+  *   'Key 4': 'Val 4',
+  *   'Key 5':  {
+  *     'Key 5.1': 'Val 5.1',
+  *     'Key 5.2': 'Val 5.2'
+  *   }
+  * }
+  * */
+
+  //This encoder is just a nice trick to 'pretty print' json
+  var encoder = JsonEncoder.withIndent('  ');
+  print(encoder.convert(mock));
+
 }
--- lib/src/set_value_base.dart
+++ lib/src/set_value_base.dart
@@ -1,6 +1,85 @@
-// TODO: Put public facing types in this file.
+class SetValue {
+  SetValue();
 
-/// Checks if you are awesome. Spoiler: you are.
-class Awesome {
-  bool get isAwesome => true;
+  Map<String, dynamic> set(
+    Map<String, dynamic> target,
+    List<String> path,
+    dynamic value,
+  ) {
+    if (!(path is List<String>)) {
+      return target;
+    }
+
+    if (path.length == 1) {
+      _result(target, path[0], value);
+      return target;
+    }
+
+    var orig = target;
+    var len = path.length;
+
+    for (var i = 0; i < len; i++) {
+      var prop = path[i];
+
+      if (!(target[prop] is Map<String, dynamic>)) {
+        target[prop] = <String, dynamic>{};
+      }
+
+      if (i == len - 1) {
+        _result(target, prop, value);
+        break;
+      }
+
+      target = target[prop];
+    }
+
+    return orig;
+  }
+
+  Map<String, dynamic> unset(
+      Map<String, dynamic> target,
+      List<String> path,
+      ) {
+    if (!(path is List<String>)) {
+      return target;
+    }
+
+    if (path.length == 1) {
+      target.remove(path[0]);
+      return target;
+    }
+
+    var orig = target;
+    var len = path.length;
+
+    for (var i = 0; i < len; i++) {
+      var prop = path[i];
+
+//      if (!(target[prop] is Map<String, dynamic>)) {
+//        target[prop] = <String, dynamic>{};
+//      }
+
+      if (!(target.containsKey(prop))) {
+        break;
+      }
+
+      if (i == len - 1) {
+        target.remove(path[i]);
+        break;
+      }
+
+      target = target[prop];
+    }
+
+    return orig;
+  }
+
+  void _result(
+    Map<String, dynamic> target,
+    String path,
+    dynamic value,
+  ) {
+    target[path] = value;
+  }
+
 }
